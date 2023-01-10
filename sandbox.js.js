79a4dcdf47f6a6cedbab018aec76324252ddd716
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-10 22:37:49 +0300

    Update sandbox.js.js

--- sandbox.js.js
+++ sandbox.js.js
@@ -63,3 +63,10 @@ let catName = "Oliver";
 let catSound = "Meow!";
 
 _________________________________________________
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+_________________________________________________
