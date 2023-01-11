35212458d3df5d4024b6b8eba85298511878e5d2
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-11 20:15:31 +0300

    Update sandbox.js.js

--- sandbox.js.js
+++ sandbox.js.js
@@ -154,3 +154,33 @@ a -= 6;
 b -= 15;
 c -= 1;
 _________________________________________________
+Compound Assignment With Augmented Multiplication
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3 ;
+c *= 10;
+_________________________________________________
+Compound Assignment With Augmented Division
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+_________________________________________________
+Escaping Literal Quotes in Strings
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+_________________________________________________
+Quoting Strings with Single Quotes
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+_________________________________________________
