650bed2b3f58d80262462f442657f59e1cc65c81
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-12 20:23:26 +0300

    Update sandbox.js.js

--- sandbox.js.js
+++ sandbox.js.js
@@ -419,3 +419,48 @@ myLocalScope();
 // myVar is not defined outside of myLocalScope
 console.log('outside myLocalScope', myVar);
 _________________________________________________
+Global vs. Local Scope in Functions
+
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  let outerWear = "sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+_________________________________________________
+Understanding Undefined Value returned from a Function
+
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+_________________________________________________
+Assignment with a Returned Value
+
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
+_________________________________________________
