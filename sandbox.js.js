e25baa53aa482430fb846783b82fc3d32bfc1307
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-12 20:11:02 +0300

    Update sandbox.js.js

--- sandbox.js.js
+++ sandbox.js.js
@@ -382,3 +382,40 @@ function timesFive (num) {
 
 const answer = timesFive (5);
 _________________________________________________
+Global Scope and Functions
+
+// Declare the myGlobal variable below this line
+let myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal here
+   oopsGlobal = 5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+_________________________________________________
+
+Local Scope and Functions
+
+function myLocalScope() {
+  // Only change code below this line
+  var myVar = "textVar";
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
+_________________________________________________
