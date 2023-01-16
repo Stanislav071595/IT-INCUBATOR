d3b0b848c56e92084a5a4045d3d99c246b20e094
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-16 20:52:49 +0300

    Update sandbox.js.js

--- sandbox.js.js
+++ sandbox.js.js
@@ -679,3 +679,25 @@ function orderMyLogic(val) {
 orderMyLogic(7);
 _________________________________________________
 
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  }else if (num < 10){
+    return "Small";
+  }else if (num < 15) {
+    return "Medium";
+  }else if (num < 20) {
+    return "Large";
+  }else {
+    return "Huge";
+  } 
+  
+  return "Change Me";
+
+  // Only change code above this line
+}
+
+testSize(7);
+_________________________________________________
+
