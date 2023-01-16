584a93260b84c21efaa3707862486e95346ef2f2
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-16 20:31:17 +0300

    Update sandbox.js.js

--- sandbox.js.js
+++ sandbox.js.js
@@ -629,3 +629,22 @@ function testLogicalOr(val) {
 testLogicalOr(15);
 _________________________________________________
 
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+  else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+_________________________________________________
+
