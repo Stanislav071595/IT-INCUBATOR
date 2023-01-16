58e66183ceedb63e484669676cca397e15b2b2fb
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-16 20:26:42 +0300

    Update sandbox.js.js

--- sandbox.js.js
+++ sandbox.js.js
@@ -614,5 +614,18 @@ function testLogicalAnd(val) {
 testLogicalAnd(10);
 _________________________________________________
 
+function testLogicalOr(val) {
+  // Only change code below this line
 
+  if (val < 10 || val > 20) {
+
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
+_________________________________________________
 
