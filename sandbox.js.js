4eeaf21ba5352fec0f0e977f5ba9567a63d93019
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-16 20:14:23 +0300

    Update sandbox.js.js

--- sandbox.js.js
+++ sandbox.js.js
@@ -553,5 +553,20 @@ function testGreaterThan(val) {
 testGreaterThan(10);
 _________________________________________________
 
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
+_________________________________________________
+
 
 
