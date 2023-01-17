874e38bf8c72857bad747ce1e29b88098a78f530
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-17 21:35:45 +0300

    Update sandbox.js.js

--- sandbox.js.js
+++ sandbox.js.js
@@ -853,3 +853,15 @@ const myDog = {
 };
 _________________________________________________
 
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+_________________________________________________
+
