7b68d6b2d598d54d6623abb006231492273190b5
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-18 19:59:33 +0300

    Update sandbox.js.js

--- sandbox.js.js
+++ sandbox.js.js
@@ -910,3 +910,15 @@ const myDog = {
 myDog["bark"] = "woof";
 _________________________________________________
 
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+delete myDog["tails"];
+// Only change code below this line
+_________________________________________________
+
