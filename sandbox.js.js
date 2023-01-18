4fdb6317435ca1505f464947b774ae4e9195c13a
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-18 19:50:02 +0300

    Update sandbox.js.js

--- sandbox.js.js
+++ sandbox.js.js
@@ -877,3 +877,15 @@ const entreeValue = testObj["an entree"];   // Change this line
 const drinkValue = testObj["the drink"];    // Change this line
 _________________________________________________
 
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+_________________________________________________
+
