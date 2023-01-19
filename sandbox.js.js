8616641bef4ec80312068434599845e355523692
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-19 20:09:44 +0300

    Update sandbox.js.js

--- sandbox.js.js
+++ sandbox.js.js
@@ -1050,6 +1050,13 @@ for (var i = 9; i > 0; i -= 2){
 // Only change code below this line
 _________________________________________________
 
-
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+}
+// Only change code below this line
+_________________________________________________
 
 
