edba642e16bd25b0d86facd0d6e0fedf1bde1943
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-19 19:29:55 +0300

    Update sandbox.js.js

--- sandbox.js.js
+++ sandbox.js.js
@@ -975,6 +975,22 @@ const myMusic = [
 ];
 _________________________________________________
 
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
+_________________________________________________
+
+
 
 
  
