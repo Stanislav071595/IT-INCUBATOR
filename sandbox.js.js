b08a40931c88536d90ff1128812ef390adf7842d
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-23 21:10:44 +0300

    Update sandbox.js.js

--- sandbox.js.js
+++ sandbox.js.js
@@ -1096,3 +1096,33 @@ function randomRange(myMin, myMax) {
 }
 _________________________________________________
 
+function convertToInteger(str) {
+   return parseInt(str);
+}
+
+convertToInteger("56");
+_________________________________________________
+
+function convertToInteger(str) {
+   return parseInt(str, 2);
+}
+
+convertToInteger("10011");
+_________________________________________________
+
+function checkEqual(a, b) {
+   return a == b ? "Equal" : "Not Equal";
+}
+
+checkEqual(1, 2);
+_________________________________________________
+
+function checkSign(num) {
+   return (num == 0) ? "zero"
+   : (num < 0) ? "negative"
+   : "positive";
+}
+
+checkSign(10);
+_________________________________________________
+
